835cadd1ce14d64f54503a62864492c9112d352a
Jorge <jsanchezberrocoso@gmail.com>
2023-12-06 14:49:11 +0100

    Descargar pdf

--- TrabajoFinGrado/Frontend/Frontend-Web/src/app/components/misreservas/misreservas.component.ts
+++ TrabajoFinGrado/Frontend/Frontend-Web/src/app/components/misreservas/misreservas.component.ts
@@ -4,6 +4,9 @@ import { MatDialog } from '@angular/material/dialog';
 import { ReservaDto } from 'src/app/models/reserva/reserva-dto';
 import { UtilsService } from 'src/app/services/utils.service';
 import { DialogAnimationsComponent } from '../automoviles/dialog-animations/dialog-animations.component';
+import jsPDF from 'jspdf';
+import { UserDto } from 'src/app/models/user/user-dto/user-dto';
+import { AutomovilDto } from 'src/app/models/automovil/automovil-dto/automovil-dto';
 
 @Component({
   selector: 'app-misreservas',
@@ -11,14 +14,18 @@ import { DialogAnimationsComponent } from '../automoviles/dialog-animations/dial
   styleUrls: ['./misreservas.component.css']
 })
 export class MisreservasComponent {
+  public usuario : UserDto;
+  public automovil : AutomovilDto | undefined = new AutomovilDto();
   public reservas : ReservaDto[]
   public reserva : ReservaDto;
   public isReservasIsNull = false;
+  httpClient: any;
 
   
   constructor(private httpService: HttpClient, private utilsService : UtilsService, public dialog: MatDialog){
     this.reservas = [];
     this.reserva = new ReservaDto();
+    this.usuario = new UserDto();
    
   }
 
@@ -29,6 +36,7 @@ export class MisreservasComponent {
       id = '0'
     }
     this.getReservasAllByClienteId(id);
+    this.findUsuario(id);
   }
 
 
@@ -96,4 +104,98 @@ export class MisreservasComponent {
     }
     this.getReservasAllByClienteId(id);
   }
+
+
+    async generarPDF(reservaa : ReservaDto) {
+
+      this.automovil = await this.findAutmovil(reservaa.automovilId);
+
+      const doc = new jsPDF();
+      doc.setFontSize(22); // Tamaño de fuente grande
+      doc.setFont('helvetica', 'bold'); // Fuente en negrita
+      // Añade contenido al PDF
+      doc.text('Reserva', 10, 10);
+      doc.setFontSize(12);
+      doc.setFont('helvetica', 'normal');
+      doc.text(`ID Reserva:  ${reservaa.id}`, 10, 20);
+
+      doc.setFontSize(16); // Tamaño de fuente grande
+      doc.setFont('helvetica', 'bold'); // Fuente en negrita
+      doc.text('Cliente', 10, 60);
+
+
+      doc.setFontSize(12);
+      doc.setFont('helvetica', 'normal');
+      doc.text(`Nombre del Cliente:  ${this.usuario.nombre}`, 10, 70);
+      doc.text(`Email del Cliente:  ${this.usuario.email}`, 10, 80);
+      doc.text(`Username del Cliente:  ${this.usuario.username}`, 10, 90);
+
+      doc.setFontSize(16); // Tamaño de fuente grande
+      doc.setFont('helvetica', 'bold'); // Fuente en negrita
+      doc.text('Automovil', 10, 110);
+
+
+      doc.setFontSize(12);
+      doc.setFont('helvetica', 'normal');
+      doc.text(`Marca del Automovil:  ${this.automovil!.marca}`, 10, 120);
+      doc.text(`Modelo del Automovil:  ${this.automovil!.modelo}`, 10, 130);
+      doc.text(`Color del Automovil:  ${this.automovil!.color}`, 10, 140);
+      doc.text(`Tipo de Automovil:  ${this.automovil!.tipo}`, 10, 150);
+
+
+
+
+      // Descarga el PDF
+      doc.save('reserva.pdf');
+    }
+
+    public async findUsuario(id :string){
+      const url: string = `http://localhost:6969/api/users/find/${id}`;
+      //const url: string = `http://128.140.34.184:8080/api/users/find/${id}`; 
+  
+  
+      const token = localStorage.getItem('access_token');
+  
+      if (token) {
+        const headers = new HttpHeaders({
+          Authorization: `Bearer ${token}`
+        });
+  
+        this.httpService.get(url).toPromise().then((data: any) => {
+          console.log(data);
+          this.usuario = data as UserDto;
+        }).catch((error) => {
+          console.error('Se ha producido un error al recuperar el usuario:', error);
+        });
+      }
+    }
+
+
+    private async findAutmovil(id: string): Promise<AutomovilDto | undefined> {
+      const url: string = `http://localhost:6969/api/automoviles/find/${id}`;
+      const token = localStorage.getItem('access_token');
+    
+      if (!token) {
+        console.error('Token no disponible');
+        return undefined;
+      }
+    
+      const headers = new HttpHeaders({
+        Authorization: `Bearer ${token}`
+      });
+    
+      try {
+        const response = await this.httpService.get(url, { headers }).toPromise();
+        console.log('Automovil encontrado correctamente');
+        return response as AutomovilDto;
+      } catch (error) {
+        console.error('Se ha producido un error al encontrar el automovil:', error);
+        return undefined;
+      }
+    }
+  
+    
+  
+  
+  
 }
